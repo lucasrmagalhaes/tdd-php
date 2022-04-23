c0c20f51a917e50020d4ee8994ea5fbec6b2a303
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-23 16:25:44 -0300

    Avaliando um leilão

---
+++ teste-avaliador.php
@@ -0,0 +1,23 @@
+<?php
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+
+require 'vendor/autoload.php';
+
+$leilao = new Leilao('Fiat 147 0KM');
+
+$maria = new Usuario('Maria');
+$joao = new Usuario('João');
+
+$leilao->recebeLance(new Lance($joao, 2000));
+$leilao->recebeLance(new Lance($maria, 2500));
+
+$leiloeiro = new Avaliador();
+$leiloeiro->avalia($leilao);
+
+$maiorValor = $leiloeiro->getMaiorValor();
+
+echo $maiorValor;
