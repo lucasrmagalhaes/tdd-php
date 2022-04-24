53ea2e96a7354938cf812bd947a257dfe15c932f
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-04-24 12:18:42 -0300

    Testando o novo código

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -26,7 +26,7 @@ class Avaliador
         $lances = $leilao->getLances();
 
         usort($lances, function (Lance $lance1, Lance $lance2) {
-            return $lance1->getValor() - $lance2->getValor();
+            return $lance2->getValor() - $lance1->getValor();
         });
 
         $this->maioresLances = array_slice($lances, 0, 3);
